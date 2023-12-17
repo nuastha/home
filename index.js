2ec169b061688530f68ffb4cce98b9ee855a16cd
nuastha <timilhllo.gmail.com>
2023-12-17 16:41:39 +0545

    js dark button fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const icon = document.querySelector(".fa-moon");
 mode.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
 
-  if (document.contains.classList("dark-theme")) {
+  if (document.body.classList.contains("dark-theme")) {
     icon.classList.remove("fa-moon");
     icon.classList.add("fa-sun");
   } else {
